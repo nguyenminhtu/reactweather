6a19e4498951088b545d1d6a11386a060c161861
Nguyen Tu <tuunguyen2795@gmail.com>
2017-01-14 09:19:55 +0700

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 
 var app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(function (req, res, next) {
+	if (req.headers['x-fowarded-proto'] === 'http') {
+		next();
+	} else {
+
+	}
+});
 
 app.use(express.static("public"));
 
-app.listen(8000, function() {
-	console.log("Server is running on port 8000");
+app.listen(PORT, function() {
+	console.log("Server is running on port " + PORT);
 });
