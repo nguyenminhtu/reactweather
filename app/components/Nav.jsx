eab02dedbe454c15163b19d62ae765bc76ba1297
Nguyen Tu <tuunguyen2795@gmail.com>
2017-01-14 11:36:19 +0700

    Update navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,37 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        alert(this.refs.key.value);
+    },
     render: function () {
         return (
-            <div>
-                <h2>Nav Component</h2>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>Get Weather</IndexLink>
-                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>About</Link>
-                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>Examples</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            React Weather App
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}, {color: '#000'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Search" ref="key" /></li>
+                            <li><button type="submit" className="button">Search</button></li>
+                         </ul>
+                    </form>
+                </div>
             </div>
         );
     }
